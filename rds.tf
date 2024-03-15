968856b0f538dcea9edb703e298f5e33ac38fc49
Igor Pestana <igor.pestana@greatcloudservices.com>
2024-03-15 19:13:36 -0300

    RDS Postgres

---
+++ rds.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "g73_techchallenge_db" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "12.6"
+  instance_class       = "db.t2.micro"
+  name                 = "g73-techchallenge-db"
+  username             = var.g73_techchallenge_db_username
+  password             = var.g73_techchallenge_db_password
+  parameter_group_name = "default.postgres12"
+}
